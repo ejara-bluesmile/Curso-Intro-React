d188b93a7dc1c2780d85d70f22b12284da2a68b3
bluesmile <bluesmile@localhost.localdomain>
2020-07-19 18:29:08 -0400

    Proyector basico de pagina web con React, uso de Algunas funciones y Api para la consulta de equipos de futbol

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./assets/css/bootstrap.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//importar componentes
+import Buscador from "./components/Buscador";
+import Resultado from "./components/Resultado";
+
+class App extends Component {
+  //state objeto mas importante React
+  state = {
+    termino: "",
+    imagenes: [],
+  };
+
+  //usar api con datos de busqueda
+  consultarApi = () => {
+    const termino = this.state.termino;
+    const url = `https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=${termino}`;
+
+    //console.log(url);
+    fetch(url)
+      .then((respuesta) => respuesta.json())
+      .then((resultado) => this.setState({ imagenes: resultado.teams }));
+  };
+
+  //funcion que pasara texto de input al controlador
+  //setState cambia el estado de State
+  datosBusqueda = (termino) => {
+    this.setState(
+      {
+        termino,
+      },
+      () => {
+        this.consultarApi();
+      }
+    );
+    //de aqui que conuslte la api
+  };
+  render() {
+    return (
+      <div className="container">
+        <div className="jumbotron">
+          <p className="lead text-center">Buscador de Imagenes</p>
+
+          <Buscador datosBusqueda={this.datosBusqueda} />
+        </div>
+        <Resultado imagenes={this.state.imagenes} />
+      </div>
+    );
+  }
 }
 
 export default App;
